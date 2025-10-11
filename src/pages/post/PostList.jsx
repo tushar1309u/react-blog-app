0f8040acca0517d9a7d4a13f9a0e3df878357fe3
tushar1309u <shettyt37@gmail.com>
2025-10-11 16:52:43 +0530

    Update PostList.jsx

--- src/pages/post/PostList.jsx
+++ src/pages/post/PostList.jsx
@@ -97,7 +97,7 @@ const ListPost=()=>{
                 {loading ? "Loading..." : (posts.map((post)=> <div className="post-card" key={post._id} onClick={()=>navigate(`detail-post/${post._id}`)}>
                     <h4 className="card-title">{post.title}</h4>
                     <p className="card-desc">
-                        {post.desc.substring(0,50)}
+                        {post.desc.substring(0,250)}
                     </p>
                     
                 </div>))}
